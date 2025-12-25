0df3a96bbd79720cdaa816a8e7411f3278f71973
Arnabdas12 <rontyarnabdas2222@gmail.com>
2025-12-25 20:09:41 +0530

    Update constants.py

---
+++ core/constants.py
@@ -0,0 +1,7 @@
+DEFAULT_SUMMARY_TYPE = "simple"
+
+SUMMARY_PROMPTS = {
+    "simple": "Summarize the following Terms & Conditions in short bullet points.",
+    "simplified": "Explain this Terms & Conditions in very simple easy to understand language.",
+    "risk": "Summarize Terms & Conditions and highlight risks, penalties & user responsibilities clearly."
+}
